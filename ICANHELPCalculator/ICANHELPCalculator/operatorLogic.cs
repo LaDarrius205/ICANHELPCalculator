2eae7ad60c911a9b6a1439b0bde5d18e3d97b944
Ladarrius Caldwell <LCaldwell1@student.fullsail.edu>
2021-11-21 12:38:33 -0500

    Add project files.

---
+++ ICANHELPCalculator/ICANHELPCalculator/operatorLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ICANHELPCalculator
+{
+    public static class operatorLogic 
+    {
+        public static double Logic(double value1, double value2, string myoperator)
+        {
+            double result = 0;
+            switch (myoperator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+
+            }
+            return result;
+        }
+    }
+}
